b414bb043aac903467d74256494a0654ca659ce0
Matthieu Auger <matthieua@theodo.fr>
2018-12-09 15:27:31 +0100

    Add plugin to main site

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,13 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-source-discogs",
+      options: {
+        key: "<YOUR_API_KEY_HERE>",
+        q: "yellow flowers",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
