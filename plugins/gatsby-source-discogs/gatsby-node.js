6365673b44e97f31ab773f78435fa641ee5d9d15
Matthieu Auger <matthieua@theodo.fr>
2018-12-09 15:53:00 +0100

    Put releases in Gatsby nodes

--- plugins/gatsby-source-discogs/gatsby-node.js
+++ plugins/gatsby-source-discogs/gatsby-node.js
@@ -8,6 +8,24 @@ exports.sourceNodes = (
   const { createNode } = actions
 
   delete configOptions.plugins
+
+  const processRelease = release => {
+    const nodeId = createNodeId(`discogs-release-${release.id}`)
+    const nodeContent = JSON.stringify(release)
+    const nodeData = Object.assign({}, release, {
+      id: nodeId,
+      parent: null,
+      children: [],
+      internal: {
+        type: `DiscogsRelease`,
+        content: nodeContent,
+        contentDigest: createContentDigest(release),
+      },
+    })
+
+    return nodeData
+  }
+
   // Convert the options object into a query string
   const apiOptions = queryString.stringify(configOptions)
   // Join apiOptions with the Pixabay API URL
@@ -22,7 +40,9 @@ exports.sourceNodes = (
       .then(data => {
         // For each query result (or 'hit')
         data.releases.forEach(release => {
-          console.log("Release data is:", release)
+          const nodeData = processRelease(release)
+          // Use Gatsby's createNode helper to create a node from the node data
+          createNode(nodeData)
         })
       })
   )
