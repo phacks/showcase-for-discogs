e769d046a35e88129821d7113f778fc3aeed1b39
phacks <nicolas.goutay@gmail.com>
2018-12-17 21:14:23 +0100

    small adjustments

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,20 @@ import Release from '../components/Release'
 const IndexPage = ({data}) => (
   <Layout>
     <Discography>
-      {data.allDiscogsRelease.edges.map(release => <Release release={{
-        artist: release.node.artists[0].name,
-        imageUrl: release.node.images.find(image => image.type === 'primary')
-          ? release.node.images.find(image => image.type === 'primary').uri
-          : release.node.images.find(image => image.type === 'secondary').uri,
-        title: release.node.title,
-        tracklist: release.node.tracklist.map(track => ({
-          duration: track.duration,
-          title: track.title,
-          position: track.position
-        })),
-        year: release.node.year
-      }} />)}
+      {data.allDiscogsRelease.edges.map(release => 
+        <Release release={{
+          artist: release.node.artists[0].name,
+          imageUrl: release.node.images.find(image => image.type === 'primary')
+            ? release.node.images.find(image => image.type === 'primary').uri
+            : release.node.images.find(image => image.type === 'secondary').uri,
+          title: release.node.title,
+          tracklist: release.node.tracklist.map(track => ({
+            duration: track.duration,
+            title: track.title,
+            position: track.position
+          })),
+          year: release.node.year
+        }} />)}
     </Discography>
   </Layout>
 )
